88345285929a3f591befdfb59f0ddaf3f85e49b5
Damian <damianvallejo123@gmail.com>
2022-07-02 19:23:14 -0300

    commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -58,6 +58,13 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  const usuario_nuevo =
+  {
+    nombre: nombre,
+    email: email,
+    password: password
+  }
+  return usuario_nuevo;
 
 }
 

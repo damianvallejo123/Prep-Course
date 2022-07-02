8505c6b0149dec801a1ae6061a6fe984f44f7bce
Damian <damianvallejo123@gmail.com>
2022-07-02 20:46:25 -0300

    commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -72,6 +72,14 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario[mail] !== undefined or usuario[mail] !== null)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {

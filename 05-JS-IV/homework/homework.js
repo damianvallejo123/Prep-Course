debe07a2fc89063f3316277610d006d6adb838df
Damian <damianvallejo123@gmail.com>
2022-07-02 18:01:21 -0300

    third commit homework 05

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,6 +21,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto.property = null;
+
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
